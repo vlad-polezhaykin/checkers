03ac85cc98ac0afd0d33e3a35b678a6a3741d8d0
38095 <vladmorozov@knu.ua>
2022-07-10 11:04:49 +0300

    created main file,class board, and added method which draws square

---
+++ cheaker/board.py
@@ -0,0 +1,14 @@
+import pygame
+from .constants import ROWS,GREEN,WHITE , SQUARE_SIZE
+
+class Board:
+    def __int__(self):
+        self.board = []
+        self.selected_piece = None
+        self.white_left = self.black_left = 12
+        self.white_kings = self.black_kings = 0
+    def draw_cubes(self,win):
+        win.fill(WHITE)
+        for row in range(ROWS):
+            for col in range(row %2,ROWS,2):
+                pygame.draw.rect(win,GREEN,(row*SQUARE_SIZE,col*SQUARE_SIZE,SQUARE_SIZE,SQUARE_SIZE))
---
+++ cheaker/main.py
@@ -0,0 +1,28 @@
+import pygame
+from cheaker.constants import WIDTH, HEIGHT
+from cheaker.board import Board
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Checkers")
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    board = Board()
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run= False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+        board.draw_cubes(WIN)
+        pygame.display.update()
+    pygame.quit()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
